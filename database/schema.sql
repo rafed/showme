c2e8e96d7ff6a429f2b35a011237054fe88d081a
Rafed <rafed123@yahoo.com>
2018-03-19 02:48:32 +0600

    Increased snippet size

--- database/schema.sql
+++ database/schema.sql
@@ -54,7 +54,7 @@ create table if not exists Edge(
 create table if not exists Citation_snippet(
 	id bigint not null AUTO_INCREMENT,
 	edge_id bigint (30),
-	text varchar (600),
+	text varchar (1000),
 
 	primary key (id),
 	foreign key (edge_id) references Edge(id)
