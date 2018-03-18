0f918ff9f87e88bc4562437a668a513a6390ee5d
Rafed <rafed123@yahoo.com>
2018-03-19 00:12:25 +0600

    Added rating

--- webapi/app.py
+++ webapi/app.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 
 from src.gsearch import gsearch
 from src.parsepdf import pdf
-
+from src.rating import rating
 
 app = Flask(__name__)
 CORS(app)
@@ -13,6 +13,7 @@ PREFIX = '/api'
 
 app.register_blueprint(gsearch, url_prefix=PREFIX)
 app.register_blueprint(pdf, url_prefix=PREFIX)
+app.register_blueprint(rating, url_prefix=PREFIX)
 
 @app.route('/')
 @app.route('/index')
@@ -20,4 +21,4 @@ def index():
     return 'Showme API 2018. See the documents for usage.'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=9999)
---
+++ webapi/src/rating.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+
+import json
+from src.databaseUtil import DatabaseUtil
+
+from flask import Blueprint, request
+rating = Blueprint('rating', __name__)
+
+
+@rating.route('/rating', methods=['POST'])
+def rate_relation():
+    data = request.get_json()
+
+    email = data['email']
+    edge_id = data['edge_id']
+    value = data['title']
+    
+    databaseUtil=DatabaseUtil()
+    query = "INSERT INTO Rating (email_id, edge_id, value) VALUES (%s, %s, %s) ON DUPLIATE KEY UPDATE value=%s"
+    args = (email, edge_id, value, value)
+    databaseUtil.executeCUDSQL(query, args)
+
+    return json.dumps({"msg":"done"})
+
+    
